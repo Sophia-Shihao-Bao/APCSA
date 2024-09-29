0e9f3f2870b0fca55618ab6cc6296b7cb9834720
sophia <sophia.bao20@gmail.com>
2024-09-29 19:38:11 -0400

    Added first test program

---
+++ src/U1C1.java
@@ -0,0 +1,10 @@
+/**
+ * @author : sophia
+ * @Description :
+ * @created : 2024-09-29,星期日
+ **/
+public class U1C1 {
+    public static void main(String[] arguments) {
+        System.out.println("Random test thingy");
+    }
+}
